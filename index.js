e6766ca3cadfb5c45679ad7cc61f352b7a06cb1d
James Masters <James@Mast3rs.com>
2019-04-04 13:55:51 +0100

    Version 2 Update.
    Adding Multiple words with some more complexatiy.

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
- 
+
 var http = require('http');
+var fs = require("fs");
 
 http.createServer(function (req, res) {
-    
-	
-	var fs = require("fs");
-	
+
 	var page = fs.readFileSync("index.html").toString();
-	
+
 	var words = fs.readFileSync("words.txt").toString();
 	words = words.split("\n")
 
-	
-	while (1 == 1){
-		var randomNumberOne = Math.floor(Math.random() * 370087);
-		var randomNumberTwo = Math.floor(Math.random() * 370087);
-		
-		var len = words[randomNumberOne].length + words[randomNumberTwo].length;
-		
-		if (len == 15){
-			break;
-		};
-	}
-	
-	
-    res.end(page + "<article><h1>" + words[randomNumberOne] + " " + words[randomNumberTwo] + "</h1></article>");
+  var special = ["!","@","#","$","%","&","?"];
+
+  for(i = 0; i < words.length; i++){
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+
+  var middleVAR = [];
+  middleVAR.push(Math.floor(Math.random() * 9999));
+  middleVAR.push(Math.floor(Math.random() * 9999));
+  middleVAR.push(special[Math.floor(Math.random()*7)]);
+  middleVAR.push(special[Math.floor(Math.random()*7)]);
+  middleVAR.push(special[Math.floor(Math.random()*7)] + Math.floor(Math.random() * 9999) + special[Math.floor(Math.random()*7)]);
+
+  var passwords = "";
+  var randomNumberOne;
+  var randomNumberTwo;
+  var len;
+
+  for(i = 0; i < 5; i++){
+
+  	while ( !(len == 15) ){
+  		randomNumberOne = Math.floor(Math.random() * 10000);
+  		randomNumberTwo = Math.floor(Math.random() * 10000);
+
+  		len = words[randomNumberOne].length + words[randomNumberTwo].length;
+  	}
+    len = 0;
+
+    passwords = passwords + "<article><h1>" + words[randomNumberOne] + " " + middleVAR[i] + " " + words[randomNumberTwo] + "</h1></article>";
+
+  }
+
+
+    res.end(page + passwords);
 }).listen(8080);
